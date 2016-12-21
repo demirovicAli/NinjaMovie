26e77dda21c5cc77fadb25b783b9bec4bf563299
demirovicAli <pokerobuka@gmail.com>
2016-12-21 17:40:22 +0100

    dodan najpopularniji.states , temaplate, state ubacen u html

---
+++ app/najpopularniji/najpopularniji.states.js
@@ -0,0 +1,10 @@
+angular
+    .module('ninja.najpopularniji')
+    .config(function($stateProvider) {
+        $stateProvider
+            .state('najpopularniji', {
+                url: '/najpopularniji',
+                templateUrl: 'app/najpopularniji/templates/najpopularniji.html',
+                controller: 'NajpopularnijiCtrl'
+            });
+        });
