958e56fcabf9c8e0098509acc1ec506e567c613a
demirovicAli <pokerobuka@gmail.com>
2016-12-22 16:34:39 +0100

    stilizirane slike

--- app/constants.js
+++ app/constants.js
@@ -6,4 +6,4 @@ angular
   // .constant('API_BASE_URL', "https://ninja-phonebook-api.herokuapp.com");
   .constant('API_BASE_URL', "https://api.themoviedb.org/3/")
   .constant('API_KEY', "api_key=5f1e3fe7439c1c65345fc1ba307278c4")
-  .constant('IMG_URL', "  http://image.tmdb.org/t/p/w500/");
+  .constant('IMG_URL', "  http://image.tmdb.org/t/p/w500");
