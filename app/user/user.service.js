45f6df3ed8187079e29bc0fb89c3f7af8eb35933
demirovicAli <pokerobuka@gmail.com>
2017-05-05 23:23:51 +0200

    Adding registering and login functionality

--- app/user/user.service.js
+++ app/user/user.service.js
@@ -1,6 +1,9 @@
 angular.module('ninja.user').service('userService', function ($http){
 	
 
+	// https://developers.themoviedb.org/3/authentication
+	// registracijq
+
 this.getToken = function (){
 
 	  return $http.get(' https://api.themoviedb.org/3/authentication/token/new?api_key=5f1e3fe7439c1c65345fc1ba307278c4')
